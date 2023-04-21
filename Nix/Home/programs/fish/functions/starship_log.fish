bb330905f5a96f1f5655eedfb715ac32abc0423a
Paul Roden Henderson <git@paulroden.net>
2023-04-21 10:51:17 +0100

    starship log function is short enough; move to embedded string

--- Nix/Home/programs/fish/functions/starship_log.fish
+++
@@ -1,9 +0,0 @@
-function starship_log
-    set -q STARSHIP_CACHE; or set STARSHIP_CACHE ~/.starship/cache
-    set session_log_file "$STARSHIP_CACHE/session_$STARSHIP_SESSION_KEY.log"
-    if test -e $session_log_file
-        bat $session_log_file
-    else
-        echo "No Starship Log for current session ($STARSHIP_SESSION_KEY)";
-    end
-end
