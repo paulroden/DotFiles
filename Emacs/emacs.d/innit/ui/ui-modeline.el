4286768c24b368986d56ed216e3a85b867dd0a64
Paul Roden Henderson <git@paulroden.net>
2024-04-24 21:37:22 +0100

    prettier pretty mode line

--- Emacs/emacs.d/innit/ui/ui-modeline.el
+++ Emacs/emacs.d/innit/ui/ui-modeline.el
@@ -4,13 +4,18 @@
 ;;; Code:
 
 ;; Modeline Styling
-;; (custom-set-faces
-;;  `(mode-line
-;;    ((t (:family "SF Mono" :size 120 :box (:line-width 3)))))
-;;  `(mode-line-inactive
-;;    ((t (:family "SF Mono" :size 120 :box (:line-width 3)))))
-;;  `(mode-line-highlight
-;;    ((t (:family "SF Mono" :size 120 :box (:line-width 3))))))
+(let ((family "SF Mono")
+      (weight 'semi-light)
+      (size  130))
+  (custom-set-faces
+   `(mode-line
+     ((t (:family ,family :weight ,weight :size ,size :box (:line-width (0 . 1))))))
+   `(mode-line-active
+     ((t (:family ,family :weight ,weight :size ,size :box (:line-width (0 . 1))))))
+   `(mode-line-inactive
+     ((t (:family ,family :weight ,weight :size ,size :box (:line-width (0 . 1))))))
+   `(mode-line-highlight
+     ((t (:family ,family :weight ,weight :size ,size :box (:line-width (0 . 1))))))))
 
 
 (use-package all-the-icons
@@ -30,35 +35,13 @@
   (lambda-line-gui-rw-symbol  " ◯ ")
   (lambda-line-gui-mod-symbol " ⊙ ")
   (lambda-line-gui-ro-symbol  " ● ")
+  (lambda-line-symbol-position -0.05)
+  (lambda-line-vc-symbol " 􀙠 ")
   :custom-face
   (lambda-line-active-status-RW ((t (:foreground "#2f9961"))))
   (lambda-line-active-status-RO ((t (:foreground "#f7dc66"))))
   (lambda-line-active-status-MD ((t (:foreground "#f1184c"))))
-  ;; (lambda-line-prefix nil)
-  ;; 
-  ;; (lambda-line-git-diff-mode-line t)
-  ;; (lambda-line-gui-rw-symbol  " ◯ ")
-  ;; (lambda-line-gui-mod-symbol " ⊙ ")
-  ;; (lambda-line-gui-ro-symbol  " ● ")
   :init (lambda-line-mode))  ;; why init works here, but not config, I am not sure.
-  ;; :custom
-  ;; (lambda-line-position 'bottom) ;; Set position of status-line
-  ;; ;; (lambda-line-abbrev t) ;; abbreviate major modes
-  ;; ;; (lambda-line-hspace "  ")  ;; add some cushion
-  ;; (lambda-line-prefix t) ;; use a prefix symbol
-  ;; ;; (lambda-line-prefix-padding nil) ;; no extra space for prefix
-  ;; ;; (lambda-line-status-invert nil)  ;; no invert colors
-  ;; ;; (lambda-line-gui-ro-symbol  " ⨂") ;; symbols
-  ;; ;; (lambda-line-gui-mod-symbol " ⬤")
-  ;; ;; (lambda-line-gui-rw-symbol  " ◯")
-
-  ;; (lambda-line-symbol-position 0.1) ;; adjust the vertical placement of symbol
-  ;; :config
-  ;; ;; activate lambda-line
-  ;; (progn
-  ;;   (message "lambda-line-mode available")
-  ;;   (lambda-line-mode)))
-
 ;; ;; (progn
 ;; ;;   (message "lambda-line-mode available")
 ;; ;;   (lambda-line-mode))
