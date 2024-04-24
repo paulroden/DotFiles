4bd5c2e1739b6a1f4300a7cd19c16891d94c83ce
Paul Roden Henderson <git@paulroden.net>
2024-04-24 21:35:20 +0100

    interim fixes for zsh config (cf. GitHub #219)

--- Nix/Home/programs/zsh.nix
+++ Nix/Home/programs/zsh.nix
@@ -1,21 +1,24 @@
-{ pkgs }:
+{ pkgs, config }:
 let
   dotDir = ".config/zsh";
 in {
   enable = true;
-  enableCompletion = false;
+  enableCompletion = true;
   enableAutosuggestions = true;
   inherit dotDir;
   history = {
     size       = 50000;
     save       = 500000;
-    path       = "${dotDir}/history";
+    path       = "${config.xdg.dataHome}/${dotDir}/history";
     ignoreDups = true;
     share      = true;
     extended   = true;
   };
   initExtraBeforeCompInit = ''
     eval "$(/opt/homebrew/bin/brew shellenv)"
+  '';
+  initExtra = ''
+    export PATH="/Users/paul/.nix-profile/bin:/run/current-system/sw/bin/:/Users/paul/.cargo/bin:/Users/paul/.cabal/bin:/Users/paul/.ghcup/bin:/nix/var/nix/profiles/default/bin:/Users/paul/.orbstack/bin:/Users/paul/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Developer/CommandLineTools/usr/bin:/Library/Apple/usr/bin"
   '';
   plugins = [ ];
 }
