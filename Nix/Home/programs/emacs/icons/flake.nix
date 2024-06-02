156df963da5456b10ef7af46e9c654c83b9cb645
Paul Roden Henderson <git@paulroden.net>
2024-06-02 19:51:50 +0100

    WIP: emacs icon overlay via flake..

--- Nix/Home/programs/emacs/icons/flake.nix
+++ Nix/Home/programs/emacs/icons/flake.nix
@@ -1,41 +1,42 @@
 {
-  description = "Nobuyuki Sato's Squricley-beautiful Emacs Icon, Derived for Building Emacs";
+  description =
+    "Nobuyuki Sato's Squricley-beautiful Emacs Icon, Derived for Building Emacs";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url =
+      "github:NixOS/nixpkgs/5e39a5c1129d6a772175418025f51b0c3022f971";
+    flake-utils.url =
+      "github:numtide/flake-utils/b1d9ab70662946ef0850d488da1c9019f3a9752a";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "aarch64-darwin";
-      pkgs = import nixpkgs { inherit system; };
-    in
-      {
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
         packages = {
           default = pkgs.stdenv.mkDerivation {
             name = "nobu417-emacs-icon";
-
             src = pkgs.fetchgit {
-              url = "https://github.com/nobu417/emacs-icon-replacement-for-macos-big-sur.git";
+              url =
+                "https://github.com/nobu417/emacs-icon-replacement-for-macos-big-sur.git";
               rev = "67630420d3fc979d4deef8e38b8426d6ad9c6256";
-              sha256 = "abc";
+              sha256 = "0lwdk697229931blhyzz498c75gm2h27w9b0m8fk4973fvn2k4mw";
             };
 
-            buildPhase = ''
-            mkdir -p $out/icons
-            cp ${src}/Emacs.icns $out/icons/
+            nativeBuildInputs = [ pkgs.imagemagick ];
 
-            source_png=$out/emacs.png
-            sizes=(16 24 32 48 128)
-            for size in "${sizes[@]}"; do
-              output_to="$out/icons/${size}x${size}/apps"
-              mkdir -p "${output_to}"
-              convert "$out/emacs.png" -resize "${size}x${size}" "${output_to}"/apps/emacs.png   
-            done
-          '';          
+            buildPhase = ''
+              mkdir -p $out/icons
+              cp $src/Emacs.icns $out/icons/
+              cp $src/emacs.png $out/icons/
+
+              mkdir -p "$out/icons/16x16/apps"; convert $src/emacs.png -resize 16x16 "$out/icons/16x16/apps/emacs.png"
+              mkdir -p "$out/icons/24x24/apps"; convert $src/emacs.png -resize 24x24 "$out/icons/24x24/apps/emacs.png"
+              mkdir -p "$out/icons/32x32/apps"; convert $src/emacs.png -resize 32x32 "$out/icons/32x32/apps/emacs.png"
+              mkdir -p "$out/icons/48x48/apps"; convert $src/emacs.png -resize 48x48 "$out/icons/48x48/apps/emacs.png"
+              mkdir -p "$out/icons/128x128/apps"; convert $src/emacs.png -resize 128x128 "$out/icons/128x128/apps/emacs.png"
+            '';
           };
         };
-      }
-
-
+      });
 }
